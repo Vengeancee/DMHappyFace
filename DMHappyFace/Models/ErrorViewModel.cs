9b47849067932ae55f2525bfaeeec8ba4fe4347d
Manish <m-kanhai@hotmail.com>
2018-04-04 15:08:55 +0200

    .Net Core

---
+++ DMHappyFace/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DMHappyFace.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
